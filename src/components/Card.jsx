3cec89f9ac2a3944cad868dbe545f70d5af74d79
Anh Tuan <anhtuan128@gmail.com>
2022-10-30 20:19:32 +1100

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import perfumeDesktop from '../images/image-product-desktop.jpg';
+import perfumeMobile from '../images/image-product-mobile.jpg';
+import addToCart from '../images/icon-cart.svg';
+import './Card.scss';
+
+export class Card extends Component {
+  width = 0;
+
+  windowWidth = () => { 
+    this.width = window.innerWidth;
+    this.setState({"width": this.width})
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.windowWidth)
+  }
+
+  render() {
+    return (
+      <div className='card'>
+        <div className="card__image">
+          <img src={this.width > 375 ? perfumeDesktop : perfumeMobile} alt="perfume" />
+        </div>
+        <div className="card__content">
+          <h2 className="card__content--type">PERFUME</h2>
+          <h1 className="card__content--title">Gabrielle Essence Eau De Parfum</h1>
+          <p>A floral, solar and voluptuous interpretation composed by Olivier Polge,
+            Perfumer-Creator for the House of CHANEL.</p>
+          <div className="card__content--price">
+            <h3>$149.99</h3>
+            <h4>$169.99</h4>
+          </div>
+          <button>
+            <img src={addToCart} alt="add to cart" />
+            <span>Add to Cart</span>
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Card;
